6a2eb260d14eac03e9f154538033328032eb94b8
VVVVictorJ <VictorRobin1097@gmail.com>
2024-10-10 20:53:58 +0800

    使用插件持久化浏览器缓存pz_v3pz

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,11 @@
 import { createStore } from 'vuex'
+import createPersistedstate from 'vuex-persistedstate'
 import menu from './menu'
 
 export default createStore({
+    plugins: [new createPersistedstate({
+        key: 'pz_v3pz',
+    })],
     modules: {
         menu
     },
