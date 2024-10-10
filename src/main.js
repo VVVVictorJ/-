62ab7ffaf520f3195590b6ce623da081b7d1bf28
VVVVictorJ <VictorRobin1097@gmail.com>
2024-10-10 20:45:37 +0800

    从缓存中获取路由数据

--- src/main.js
+++ src/main.js
@@ -1,28 +1,36 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import panelHead from './components/panelHead.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import panelHead from "./components/panelHead.vue";
 import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 
+const localData = localStorage.getItem("pz_v3pz");
+if (localData) {
+  store.commit("dynamicMenu", JSON.parse(localData).menu.routerList);
+  store.state.menu.routerList.forEach((item) => {
+    router.addRoute('main',item);
+  })
+}
+
 router.beforeEach((to, from) => {
-  const token = localStorage.getItem('pz_token')
-  if (!token && to.path !== '/login') {
-    return '/login'
-  } else if (token && to.path === '/login') {
-    return '/'
+  const token = localStorage.getItem("pz_token");
+  if (!token && to.path !== "/login") {
+    return "/login";
+  } else if (token && to.path === "/login") {
+    return "/";
   } else {
-    return true
+    return true;
   }
-})
+});
 
 const app = createApp(App);
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component);
 }
-app.component('panelHead', panelHead)
+app.component("panelHead", panelHead);
 app.use(router);
 //store
 app.use(store);
-app.mount('#app');
+app.mount("#app");
